e9b27d95ce22a637ca671504aed076712290b5a1
Damon <damondias@gmail.com>
2023-07-03 14:41:44 -0400

    fix: setInterval middleware

--- src/app.js
+++ src/app.js
@@ -3,8 +3,9 @@ import dotenv from "dotenv";
 import cors from "cors";
 import router from "./Routers/index.js";
 
-import db from "./database.js";
-import dayjs from "dayjs"
+// import db from "./database.js";
+// import dayjs from "dayjs"
+import validateInactivesMiddleware from "./Middlewares/validateInactivesMiddleware.js";
 
 dotenv.config();
 
@@ -14,34 +15,35 @@ app.use(cors());
 app.use(json());
 app.use(router);
 
-setInterval(async () => {
-    try {
-    const lastTenSeconds = Date.now() - 10000;
-    const participants = await db.collection("participants").find().toArray();
+// setInterval(async () => {
+//     try {
+//     const lastTenSeconds = Date.now() - 10000;
+//     const participants = await db.collection("participants").find().toArray();
 
-    const inactiveParticipants = participants.filter(participant => participant.lastStatus <= lastTenSeconds)
-    if (inactiveParticipants.length === 0) {
-        return;
-    }
+//     const inactiveParticipants = participants.filter(participant => participant.lastStatus <= lastTenSeconds)
+//     if (inactiveParticipants.length === 0) {
+//         return;
+//     }
 
-    await db.collection("participants").deleteMany({ lastStatus: { $lte: lastTenSeconds } });
+//     await db.collection("participants").deleteMany({ lastStatus: { $lte: lastTenSeconds } });
 
-    const inactiveParticipantsMessages = inactiveParticipants.map(participant => {
-        return {
-        from: participant.name,
-        to: 'Todos',
-        text: 'sai da sala...',
-        type: 'status',
-        time: dayjs().format("HH:mm:ss")
-        }
-    })
+//     const inactiveParticipantsMessages = inactiveParticipants.map(participant => {
+//         return {
+//         from: participant.name,
+//         to: 'Todos',
+//         text: 'sai da sala...',
+//         type: 'status',
+//         time: dayjs().format("HH:mm:ss")
+//         }
+//     })
 
-    await db.collection("messages").insertMany(inactiveParticipantsMessages);
+//     await db.collection("messages").insertMany(inactiveParticipantsMessages);
 
-    } catch (error) {
-    console.log(error);
-    }
-}, 15000);
+//     } catch (error) {
+//     console.log(error);
+//     }
+// }, 15000);
+validateInactivesMiddleware();
 
 const PORT = process.env.PORT || 5000;
 
