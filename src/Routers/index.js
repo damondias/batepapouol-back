bdc67a28d139ea90b2bb89ff11b6a895d0ab103b
Damon <damondias@gmail.com>
2023-07-03 14:05:01 -0400

    fix: setInterval

--- src/Routers/index.js
+++ src/Routers/index.js
@@ -1,11 +1,8 @@
 import { Router } from "express";
 import participantsRouter from "./participantsRouter.js";
 import messagesRouter from "./messagesRouter.js";
-import validateInactivesMiddleware from "../Middlewares/validateInactivesMiddleware.js";
 
 const router = Router();
-
-router.use(validateInactivesMiddleware);
 
 router.use(participantsRouter);
 router.use(messagesRouter);
